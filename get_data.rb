cc24f523311a39f9a8cffef66359a9f33a773fae
mdudzinski <dudzinski.michal@yahoo.com>
2012-03-27 01:26:24 +0200

    Dodane zapisywanie do bazy danych mongodb.

--- get_data.rb
+++ get_data.rb
@@ -1,15 +1,17 @@
 require 'net/http'
-require 'json'
+require 'mongo'
 
 resp = String.new
 all_raw_data_array = Array.new
-all_json_data_array = Array.new
 
 Net::HTTP.start("bash.org.pl") do |http|
     resp = http.get("/text")
 end
 
-all_raw_data_array = resp.body.force_encoding("UTF-8").split(/^%$/)
+all_raw_data_array = resp.body.encode("UTF-8", {:undef => :replace}).split(/^%$/)
+
+@db = Mongo::Connection.new("localhost", 27017).db("nosql_zaliczenie")
+@collection = @db.collection("mnostwo_bashow")
 
 all_raw_data_array.each do |bash_quote_string|
   bash_quote_json = Hash.new
@@ -19,11 +21,11 @@ all_raw_data_array.each do |bash_quote_string|
     lines = bash_quote_string.split($/, 2)
     first_line = lines.shift 
     quote = lines.shift
-    bash_quote_json[:id] = first_line.split(' ')[0] 
+    bash_quote_json[:_id] = first_line.split(' ')[0] 
     bash_quote_json[:url] = first_line.split(' ')[1] 
     bash_quote_json[:quote] = quote
-
-    puts JSON.pretty_generate(bash_quote_json)
+    
+    @collection.insert(bash_quote_json)
   end 
 end
 
